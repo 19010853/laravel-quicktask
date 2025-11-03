ada7ebeaf2297bb0bbdcb42876cc8bbe475cb535
19010853 <khoinguyenminhk37@gmail.com>
2025-11-03 17:01:14 +0700

    9. Add auth method using laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,20 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\TaskController;
-
-Route::middleware(['auth', 'admin'])->group(function () {
-    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-});
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create')->middleware(['admin']);
-Route::post('/users', [UserController::class, 'store'])->name('users.store');
-Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
-Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
-Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
-Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Optional alias to support /users/index URL
-Route::get('/users/index', [UserController::class, 'index']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::resource('/tasks', TaskController::class);
+require __DIR__.'/auth.php';
