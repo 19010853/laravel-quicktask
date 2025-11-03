89f200114ec6a0b6336d9411329a05cacdde69a2
19010853 <khoinguyenminhk37@gmail.com>
2025-11-03 16:44:11 +0700

    7. Add middlware to check if an user is admin

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,20 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\TaskController;
+
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+});
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+Route::get('/users/create', [UserController::class, 'create'])->name('users.create')->middleware(['admin']);
 Route::post('/users', [UserController::class, 'store'])->name('users.store');
 Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
 Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
